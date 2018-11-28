60946ddde4e4b6416801372c1751110083a2dccb
jdholst <35906024+jdholst@users.noreply.github.com>
2018-11-27 23:30:14 -0600

    Update Records.cs

--- Records.cs
+++ Records.cs
@@ -1,9 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SICXE
 {
+    /// <summary>
+    /// LinkedList containing methods  object code record 
+    /// </summary>
     public abstract class Record
     {
         protected LinkedList<string> record;
@@ -12,12 +15,14 @@ namespace SICXE
         private int size;
         private readonly string type;
 
+        /// <summary>
+        /// Current size of the record.
+        /// </summary>
         public int Size {
             get
             {
                 return size;
             }
-
             protected set
             {
                 if (size >= maxEntries)
@@ -35,6 +40,10 @@ namespace SICXE
             record = new LinkedList<string>();
         }
 
+        /// <summary>
+        /// String form of the record. Each entry is separated by a ^.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             StringBuilder recordString = new StringBuilder(type);
@@ -50,6 +59,9 @@ namespace SICXE
         public abstract void AddEntry(RecordArgs args);
     }
 
+    /// <summary>
+    /// First record of the object program containing the program name, starting address, and program length
+    /// </summary>
     public class HeaderRecord : Record
     {
         public HeaderRecord(string programName, string startingAddress, string programLength) : base("H", 1)
@@ -60,12 +72,19 @@ namespace SICXE
             Size++;
         }
 
+        /// <summary>
+        /// Throws exception because header only contains one entry added in the contructor.
+        /// </summary>
+        /// <param name="args"></param>
         public override void AddEntry(RecordArgs args)
         {
             throw new RecordOverflowException();
         }
     }
 
+    /// <summary>
+    /// Last record of the program containing starting address of program.
+    /// </summary>
     public class EndRecord : Record
     {
         public EndRecord(string startingAddress) : base("E", 1)
@@ -74,12 +93,20 @@ namespace SICXE
             Size++;
         }
 
+        /// <summary>
+        /// Throws exception because end record only contains one entry added in the contructor.
+        /// </summary>
+        /// <param name="args"></param>
         public override void AddEntry(RecordArgs args)
         {
             throw new RecordOverflowException();
         }
     }
 
+    /// <summary>
+    /// Begins with starting address of the record and the size of the record. 
+    /// Each object code entry is added to the end with a max size of 10 entries.
+    /// </summary>
     public class TextRecord : Record
     {
         private string location;
@@ -92,6 +119,10 @@ namespace SICXE
             record.AddLast(location.PadLeft(6, '0'));
         }
 
+        /// <summary>
+        /// Adds an entry containing object code.
+        /// </summary>
+        /// <param name="args"> Contains object-code and location counter that helps keep track of the record size </param>
         public override void AddEntry(RecordArgs args)
         {
             Size++;
@@ -104,10 +135,17 @@ namespace SICXE
         }
     }
 
+    /// <summary>
+    /// Contains names of all symbols referenced through EXTREF directive.
+    /// </summary>
     public class ReferenceRecord : Record
     {
-        public ReferenceRecord() : base("R", 10) { }
+        public ReferenceRecord() : base("R", int.MaxValue) { }
 
+        /// <summary>
+        /// Adds referenced symbol
+        /// </summary>
+        /// <param name="args"> Contains name of symbol </param>
         public override void AddEntry(RecordArgs args)
         {
             record.AddLast(args.Line);
@@ -115,10 +153,17 @@ namespace SICXE
         }
     }
 
+    /// <summary>
+    /// Contains names of all symbols defined through EXTDEF directive
+    /// </summary>
     public class DefineRecord : Record
     {
-        public DefineRecord() : base("D", 10) { }
+        public DefineRecord() : base("D", int.MaxValue) { }
 
+        /// <summary>
+        /// Adds an entry containing the symbol name and its address
+        /// </summary>
+        /// <param name="args"></param>
         public override void AddEntry(RecordArgs args)
         {
             record.AddLast(args.Line);
@@ -127,6 +172,10 @@ namespace SICXE
         }
     }
 
+    /// <summary>
+    /// Used for symbols which are modified by a format 4 instruction.
+    /// Contains address, size, and name of the modification
+    /// </summary>
     public class ModificationRecord : Record
     {
         public ModificationRecord(string address, string size, string name) : base("M", 1)
